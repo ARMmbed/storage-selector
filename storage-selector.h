79bb683d9a5402405bf182c0ca8af438c2512be4
Marcus Chang <marcus.chang@arm.com>
2017-11-01 11:53:14 -0700

    Add internal flash

--- storage-selector.h
+++ storage-selector.h
@@ -5,6 +5,7 @@
 #include "options/heap.h"
 #include "options/spi_flash.h"
 #include "options/dataflash.h"
+#include "options/internal_flash.h"
 
 // These two macros are a bit of magic that concatinate the symbol and
 // function prefix to create a valid function name
