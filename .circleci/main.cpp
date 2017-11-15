fdfbc3852a1a8767aec47e9af41f40170e35c94e
Brian Daniels <brianddaniels@gmail.com>
2017-11-15 14:29:10 -0600

    Updating tests

--- .circleci/main.cpp
+++ .circleci/main.cpp
@@ -2,13 +2,29 @@
 #include "storage-selector.h"
 
 int main() {
-    // Create both the filesystem and the block device
-    FileSystem *fs = filesystem_selector();
-
+    printf("Starting...\r\n");
     // Retrieve just the block device and get erase size
     BlockDevice *bd = storage_selector();
+    bd->init();
     volatile bd_size_t erase_size = bd->get_erase_size();
 
+    SlicingBlockDevice slice1(bd, 0*512, 32*512);
+    SlicingBlockDevice slice2(bd, 32*512, 64*512);
+
+    // Multiple filesystem example (partitions)
+    printf("Fs warnings may show here...\r\n");
+    FileSystem *fs1 = filesystem_selector(&slice1, "s1", 0);
+    FileSystem *fs2 = filesystem_selector(&slice2, "s2", 1);
+
+    printf("\r\nReformatting...\r\n");
+    fs1->reformat();
+    fs2->reformat();
+
     // Unmount fs
-    fs->unmount();
+    fs1->unmount();
+    fs2->unmount();
+
+    fs1->mount(&slice1);
+    fs2->mount(&slice2);
+    printf("Completed!\r\n");
 }
